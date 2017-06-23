30b5e6d0e70e3d99f7adc4198f717aba249c486c
Francisco José Juan Quintanilla <frnjjq@gmail.com>
2017-06-23 12:02:32 +0200

    Created neon functions
    Added neon functions. Integration with the c code is the next step

--- libavcodec/arm/lheenc_neon.h
+++ libavcodec/arm/lheenc_neon.h
@@ -4,11 +4,19 @@
  * It contains the declarations of the functions written in ARM assembly using
  * NEON instructions. Only those are declarated in the case it is compiled with NEON flag.
  * The funtion we want to modify is "lhe_basic_encode_one_hop_per_pixel_block".
- * 
+ *
  */
 
 #include "config.h"
 #include <stdint.h>
 
-  void lhe_enc_predict_from_previous_neon(uint8_t predicted_component_neon[],uint8_t component_prediction[],int pix);
-  void lhe_enc_predict_from_neighbour_neon(uint8_t predicted_component_neon[],uint8_t component_prediction[],int pix,uint32_t width);
+void calulate_hop_prediction_binary_neon( uint8_t *original_color,
+                                          uint8_t *predicted_component,
+                                          uint8_t *hop_1, uint8_t *hops,
+                                          uint8_t *component_prediction);
+
+void lhe_neon_prediction( uint8_t pixel_before[], uint8_t pixel_upafter[],
+                          uint8_t pixel_predicted[]);
+
+void lhe_h1adapt_neon( uint8_t *hop_number, uint8_t *column_hop_1,
+                       uint8_t *column_last_small_hop);
